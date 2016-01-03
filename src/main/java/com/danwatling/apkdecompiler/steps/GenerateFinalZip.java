5423b4d6ec61100dc4a13b8d100923e7dfb2667f
Dan Watling <dan@synaptik.com>
2016-01-03 17:38:31 -0600

    Final zip file is now created.

--- src/main/java/com/danwatling/apkdecompiler/steps/GenerateFinalZip.java
+++ src/main/java/com/danwatling/apkdecompiler/steps/GenerateFinalZip.java
@@ -1,5 +1,9 @@
 package com.danwatling.apkdecompiler.steps;
 
+import com.danwatling.apkdecompiler.Logger;
+import net.lingala.zip4j.core.ZipFile;
+import net.lingala.zip4j.model.ZipParameters;
+
 import java.io.File;
 
 /**
@@ -15,6 +19,36 @@ public class GenerateFinalZip extends BaseStep {
 	public boolean run() {
 		boolean result = true;
 
+		generateZipFile();
+		removeWorkFolder();
+
 		return result;
+	}
+
+	private void generateZipFile() {
+		try {
+			File outputFile = new File(this.workFolder.getName() + ".zip");
+			if (outputFile.exists()) {
+				Logger.info(outputFile.getAbsolutePath() + " already exists. Deleting.");
+				outputFile.delete();
+			}
+			ZipFile output = new net.lingala.zip4j.core.ZipFile(outputFile);
+			Logger.info("Generating " + output.getFile().getAbsolutePath());
+
+			ZipParameters params = new ZipParameters();
+			params.setIncludeRootFolder(false);
+			params.setRootFolderInZip("");
+			output.addFolder(this.workFolder.getAbsolutePath() + File.separator + "apktool", params);
+			params.setRootFolderInZip("classes");
+			output.addFolder(this.workFolder.getAbsolutePath() + File.separator + "classes", params);
+		} catch (Throwable t) {
+			Logger.error("Unable to generate final zip file.", t);
+		}
+	}
+
+	private void removeWorkFolder() {
+		if (!workFolder.delete()) {
+			Logger.info("Unable to remove " + workFolder.getAbsolutePath());
+		}
 	}
 }
